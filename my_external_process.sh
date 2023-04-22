03566e22cf74ea23f8ceba47f3efae70b84ebbb0
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2023-04-22 10:28:40 +0100

    Demonstrate process invocation timeout

--- my_external_process.sh
+++ my_external_process.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+count=$1
+for i in $(seq 1 $count)
+do
+  echo "I'm thinking $i"
+done
+echo "The answer is 42!"
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Process p = Runtime.getRuntime().exec("./my_external_process.sh 5000");
+        boolean done = p.waitFor(60, TimeUnit.SECONDS);
+        if (done) {
+            System.out.println("Process completed successfully");
+        } else {
+            System.out.println("Process did not complete in 60 seconds");
+        }
     }
 }
